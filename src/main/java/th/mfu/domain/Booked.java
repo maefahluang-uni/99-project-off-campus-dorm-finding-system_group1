cbe61a332bba332c316eb084a8873416bbbaa3af
Khant_Min_Htet 6531503147 <6531503147@lamduan.mfu.ac.th>
2023-11-20 14:04:36 +0700

    fix landlord

--- src/main/java/th/mfu/domain/Booked.java
+++ src/main/java/th/mfu/domain/Booked.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-
+ 
 @Entity
 public class Booked {
     @Id
--- src/main/java/th/mfu/domain/Landlord.java
+++ src/main/java/th/mfu/domain/Landlord.java
@@ -1,51 +1,50 @@
 package th.mfu.domain;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 @Entity
-public class Booked {
+public class Landlord 
+{
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    @ManyToOne
-    private Tenant tenant;
-    @ManyToOne
-    private Dormitory dorm;
-    @ManyToOne
-    private Landlord landlord;
-
-    public Booked()
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String telephone;
+    public Landlord()
     {
 
     }
-
-    public Tenant getTenant() {
-        return tenant;
+    public Landlord(String email, String firstName, String lastName, String telephone) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.telephone = telephone;
     }
-
-    public void setTenant(Tenant tenant) {
-        this.tenant = tenant;
+    public String getEmail() {
+        return email;
     }
-
-    public Dormitory getDorm() {
-        return dorm;
+    public void setEmail(String email) {
+        this.email = email;
     }
-
-    public void setDorm(Dormitory dorm) {
-        this.dorm = dorm;
+    public String getFirstName() {
+        return firstName;
     }
-
-    public Landlord getLandlord() {
-        return landlord;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
-
-    public void setLandlord(Landlord landlord) {
-        this.landlord = landlord;
+    public String getLastName() {
+        return lastName;
     }
-
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getTelephone() {
+        return telephone;
+    }
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+    
+    
 }
