86bad6e6d7139e6645aa0a9f46a0df0063a6c20f
Kaung Htut Hlaing <6531503145@lamduan.mfu.ac.th>
2023-11-22 16:59:17 +0700

    fixed some errors

--- src/main/java/th/mfu/Repos/WishListRepository.java
+++ src/main/java/th/mfu/Repos/WishListRepository.java
@@ -8,7 +8,7 @@ import th.mfu.domain.WishList;
 import java.util.List;
 
 
-public interface WishListRepository extends CrudRepository<WishList, String> {
+public interface WishListRepository extends CrudRepository<WishList, Integer> {
     List<WishList> findByTenant(Tenant tenant);
     void deleteByDormitory(Dormitory dormitory);
     List<WishList> findByDormitoryAndTenant(Dormitory dormitory, Tenant tenant);
--- src/main/java/th/mfu/domain/WishList.java
+++ src/main/java/th/mfu/domain/WishList.java
@@ -12,7 +12,7 @@ import javax.persistence.OneToMany;
 public class WishList {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private int id;
 
     @ManyToOne
     private Tenant tenant;
