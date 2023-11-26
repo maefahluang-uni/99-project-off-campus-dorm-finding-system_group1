4e2761cc732f355109ae0d6ace44beb25fd53b56
Fisheesh <6531503187@lamduan.mfu.ac.th>
2023-11-26 16:36:26 +0700

    alert message added

--- src/main/resources/static/js/overlay.js
+++ src/main/resources/static/js/overlay.js
@@ -18,6 +18,17 @@ book.addEventListener("click", () => section.classList.add("popup"))
 overlay1.addEventListener("click", () => section.classList.remove("popup"))
 cancel1.addEventListener("click", () => section.classList.remove("popup"))
 
+//alert message
+const bookbtn = document.querySelector(".booking")
+const okay = document.querySelector(".ok")
+
+bookbtn.addEventListener("click", ()=>  {
+    section.classList.add("alert")
+    section.classList.remove("popup")
+})
+okay.addEventListener("click", () => section.classList.remove("alert"))
+overlay1.addEventListener("click", () => section.classList.remove("alert"))
+
 // stars
 // select all elements with the "i" tag and store them in a NodeList called "stars"
 const stars = document.querySelectorAll(".star i")
